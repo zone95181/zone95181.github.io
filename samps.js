9406c8fdff70f0660abd6b61b755629d0e95d7ed
zone95181 <151130361+zone95181@users.noreply.github.com>
2024-11-02 23:38:34 +0900

    Create samps.js

---
+++ samps.js
@@ -0,0 +1,23 @@
+/*stamps*/
+/*20241102*/
+var canvas = document.createElement("canvas");
+var c = canvas.getContext("2d");
+var imglogo = new Image();
+
+imglogo.src = "logo.png? raw=true";
+
+
+function onLoad()
+{
+	c = document.getElementById("logo_image").getContext("2d");
+	c.clearRect(0, 0, imglogo.width, imglogo.height);
+	c.drawImage(imglogo, 0, 0, imglogo.width, imglogo.height);
+	c = document.getElementById("stamp_logo").getContext("2d");
+	c.clearRect(0, 0, imglogo.width, imglogo.height);
+	c.drawImage(imglogo, 0, 0, imglogo.width, imglogo.height);
+	c = null;
+}
+
+	alert();
+window.onload = onLoad;
+
